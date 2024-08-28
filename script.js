bc5206353380799dd511f2d0d2a0ffd24d09e458
Gabi-J <jaskova.gab@gmail.com>
2024-08-28 15:10:38 +0200

    upravy

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMenu() {
     icon.classList.toggle("open")
 }
 
-/* Arrow */
+/* arrow */
 
 window.addEventListener('scroll', function() {
     const arrow = document.querySelector('.arrow');
@@ -17,11 +17,38 @@ window.addEventListener('scroll', function() {
     const aboutRect = aboutSection.getBoundingClientRect();
     const footerRect = footerSection.getBoundingClientRect();
 
-    // Zobrazit šipku, když je sekce #about zobrazená nebo pokud je zobrazen footer
-    if (aboutRect.bottom <= window.innerHeight || footerRect.top <= window.innerHeight) {
+    // Zobrazit šipku, když je sekce #about zobrazená a zůstat až do konce stránky
+    if (aboutRect.bottom <= window.innerHeight && footerRect.top > window.innerHeight) {
         arrow.style.display = 'block';
     } else {
         arrow.style.display = 'none';
+    }
+});
+
+window.addEventListener('scroll', function() {
+    const arrow = document.querySelector('.message');
+    const aboutSection = document.querySelector('#about');
+    const footerSection = document.querySelector('footer');
+    
+    const aboutRect = aboutSection.getBoundingClientRect();
+    const footerRect = footerSection.getBoundingClientRect();
+
+    // Zobrazit šipku, když je sekce #about zobrazená a zůstat až do konce stránky
+    if (aboutRect.bottom <= window.innerHeight && footerRect.top > window.innerHeight) {
+        arrow.style.display = 'block';
+    } else {
+        arrow.style.display = 'none';
+    }
+});
+
+/* menu */
+
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 1) { 
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
     }
 });
 
@@ -30,11 +57,13 @@ window.addEventListener('scroll', function() {
 // Funkce pro otevření popupu
 function openPopup(id) {
     document.getElementById(id).style.display = 'block';
+    document.body.classList.add('no-scroll'); // Přidání třídy no-scroll
 }
 
 // Funkce pro zavření popupu
 function closePopup(id) {
     document.getElementById(id).style.display = 'none';
+    document.body.classList.remove('no-scroll'); // Odstranění třídy no-scroll
 }
 
 // Funkce pro otevření popupu s výsledkem (obrázek nebo PDF)
@@ -42,7 +71,6 @@ function openResultPopup(fileSrc) {
     const resultPopup = document.getElementById('popup-result');
     const resultContent = document.getElementById('result-content');
     
-    // Kontrola, zda je soubor obrázek nebo PDF
     if (fileSrc.endsWith('.pdf')) {
         resultContent.innerHTML = `<embed src="${fileSrc}" type="application/pdf"/>`;
     } else {
@@ -50,6 +78,7 @@ function openResultPopup(fileSrc) {
     }
 
     resultPopup.style.display = 'block';
+    document.body.classList.add('no-scroll'); // Přidání třídy no-scroll
 }
 
 // Funkce pro zavření popupu při kliknutí mimo jeho obsah
